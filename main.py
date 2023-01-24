3fc587cab6200b3005b49c9f1099cbe93fb2eda3
Nafiul Islam <gamesbrainiac@gmail.com>
2023-01-24 15:06:23 +0100

    Adding check for RSPEC-6465

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+def foo(param):
+    ls = [1, 2, 3]
+    x, y = ls
+    return x, y
 
 
 def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 
