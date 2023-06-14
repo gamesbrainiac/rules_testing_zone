4b712f6b2fc0c6e62aeb6955776afdb5ce1966b4
Nafiul Islam <gamesbrainiac@gmail.com>
2023-06-14 17:50:37 +0100

    Making some documentation changes.

--- 01_02_mutable_arugments_decorators.py
+++ 01_02_mutable_arugments_decorators.py
@@ -2,6 +2,10 @@ from functools import wraps
 
 
 def logger(func):
+    """
+    Logger that logs the function name and arguments
+    when it is called
+    """
     @wraps(func)
     def wrapper(*args, **kwargs):
         print(f"Calling function {func.__name__} with args {args} and kwargs {kwargs}")
@@ -11,6 +15,11 @@ def logger(func):
 
 @logger
 def add_item(item, items=[]):
+    """
+    A function that adds an item to an existing list.
+    If a list is not provided, it creates a new item
+    with that list added.
+    """
     items.append(item)
     return items
 
--- 03_01_late_binding_closures_w.py
+++ 03_01_late_binding_closures_w.py
@@ -1,8 +1,15 @@
 def times_table():
+    """
+    Returns a list of functions that will give you
+    the times table.
+
+    >>> [f(5) for f in times_table()]
+    >>> [0, 5, 10, 15, 20, 25, 30, 35, 40, 45]
+    """
     table = []
     for i in range(10):
-        def multiplier(x):
-            return i * x
+        def multiplier(x):  # This is a closure, because it has access to its outer lexical scope
+            return i * x  # lookup for the variable i happens when the variable is used, not when it defined.
 
         table.append(multiplier)
 
